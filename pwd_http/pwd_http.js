82249bcc787c0cbbfea71fdc071616e25d6a4a18
yangwe1 <weiyang512@gmail.com>
2015-11-04 21:15:13 +0800

    only listen on localhost

--- pwd_http/pwd_http.js
+++ pwd_http/pwd_http.js
@@ -3,8 +3,6 @@ var url = require('url');
 var querystring = require('querystring');
 var rsa = require('./rsa.js');
 
-// var data = {username: "mm4690", password: "tnVT`msHI1_pw:7K", captcha: "!Bkh", ke: "AQAB", kn: "uiS37CN8fZ0XfY4udgeccUjtfb73wN+sd+08EhDNj4gwMLXldQnvTCjIhOPUdFl8uCigZeSUnotolncwI5C+7gvx1MZMdbW0IBpCi2sEYYypRl+yK8EQ2GQDacPYYDs8SLXqBCcAT6BAexQnwjVvS0WsXdataDjDS1RvHj4xmSk="};
-
 http.createServer(function (request, response) {
     var q_str = url.parse(request.url).query;
     var params = querystring.parse(q_str);
@@ -24,4 +22,4 @@ http.createServer(function (request, response) {
         response.end();
         console.log(ret)
     }
-}).listen(8888)
+}).listen(8888, '127.0.0.1')
